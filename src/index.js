6ac0a995946026daac0667ead371ba1a2e392de2
Allanflo88 <allanfloresdejesus@hotmail.com>
2021-03-16 20:25:53 -0300

    move assets to src folder

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 //JS Files
 import 'jquery'
 import 'bootstrap';
-// import '../js/angular.min.js';
-// import '../js/main.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Invoice } from './components/invoice.jsx'
 
 //CSS Files
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/style.css';
+import './assets/css/style.css';
 
 //Assets Files
-import '../images/metaware_logo.png';
+import './assets/images/metaware_logo.png';
 
 
 ReactDOM.render(<Invoice />,
